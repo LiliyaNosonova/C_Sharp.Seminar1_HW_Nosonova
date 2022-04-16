16395e6484fd64192a72929d8fbc73b15bdfe406
Lnosonova <lilipadnose@gmail.com>
2022-04-16 18:06:28 +0300

    Add HW001

---
+++ HW001/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1: Напишите программу, которая на вход 
+//принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 8;
+int b = 7;
+if (a > b)
+{
+    System.Console.Write("max = "); 
+    System.Console.Write(a);
+    System.Console.WriteLine();
+    System.Console.Write("min = "); 
+    System.Console.Write(b);     
+}
+else
+{
+    System.Console.Write("max = "); 
+    System.Console.Write(b);
+    System.Console.WriteLine();
+    System.Console.Write("min = "); 
+    System.Console.Write(a);  
+}
